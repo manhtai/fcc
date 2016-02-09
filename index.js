2d9521cbbb5bd082b6bf45067e96073944e8f90b
manhtai <vo@manhtai.com>
2016-02-09 09:51:53 +0700

    Add timestamp challenge

---
+++ index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var strf = require('strftime');
+var app = express();
+app.set('port', process.env.PORT || 3000);
+
+// home page
+app.get('/timestamp/:param', function(req, res){
+    var param = req.params.param;
+    var date;
+    param = Number(param) ? Number(param) : Date.parse(param + ' UTC') / 1000;
+    if (param) {
+        date = new Date(0);
+        date.setSeconds(param);
+    } else {
+        date = null;
+    }
+    var result = {
+        "unix": date ? date.getTime() / 1000 : null,
+        "natural": date ? strf("%B %d, %Y", date) : null
+    };
+    res.type('application/json');
+    res.send(JSON.stringify(result));
+});
+
+// custom 404 page
+app.use(function(req, res, next){
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+// custom 500 page
+app.use(function(err, req, res, next){
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+var server = app.listen(app.get('port'), function(){
+    console.log( 'Express started on http://localhost:' +
+    app.get('port') + '; press Ctrl-C to terminate.' );
+});
+
+module.exports = server;
---
+++ test.js
@@ -0,0 +1,33 @@
+var request = require('supertest');
+
+suite('Timestamp Microservice Tests', function(){
+    var server;
+    setup(function () {
+        // Flush cache
+        delete require.cache[require.resolve('./index')];
+        server = require('./index');
+    });
+    teardown(function (done) {
+        server.close(done);
+    });
+
+    var result = {"unix":1450137600, "natural":"December 15, 2015"};
+    var null_result = {"unix":null, "natural":null};
+    test('should be able to convert natural date', function(done){
+        request(server)
+        .get('/timestamp/December 15, 2015')
+        .expect(200, result, done);
+    });
+
+    test('should be able to convert Unix date', function(done){
+        request(server)
+        .get('/timestamp/1450137600')
+        .expect(200, result, done);
+    });
+
+    test('should return null on error parsing', function(done){
+        request(server)
+        .get('/timestamp/foo')
+        .expect(200, null_result, done);
+    });
+});
